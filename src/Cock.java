87a212c9d18b4c2753565a54d04803205dbb1b23
LAPTOP-Kenneth <kennethdammortensen@gmail.com>
2019-03-28 13:46:24 +0100

    Oprettet

---
+++ src/Cock.java
@@ -0,0 +1,4 @@
+public class Cock {
+    double length;
+}
+
--- src/MathCockStuff.java
+++ src/MathCockStuff.java
@@ -1,4 +1,9 @@
 public class MathCockStuff
 {
-    public int add
+    public Cock addCocks(Cock cock1, Cock cock2){
+
+        Cock cockReturn = new Cock();
+        cockReturn.length = (cock1.length + cock2.length);
+        return cockReturn;
+    }
 }
